6925d579fc386827262616b025fb3a9e33867d1c
sudokufan <aday@hey.com>
2025-01-30 14:50:40 +0000

    headings on mobile

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -9,9 +9,30 @@ import {
   Link,
 } from "@heroui/react";
 import {useState} from "react";
+import {useLocation} from "react-router-dom";
 
 export const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const location = useLocation();
+  const currentPath = location.pathname;
+
+  const getLinkClass = (path: string, defaultColor: string) =>
+    currentPath === path ? "text-gold" : defaultColor;
+
+  const getPageTitle = () => {
+    switch (currentPath) {
+      case "/dashboard":
+        return "Dashboard";
+      case "/settings/connections":
+        return "Connections";
+      case "/settings/invite":
+        return "Invite";
+      case "/settings":
+        return "Settings";
+      default:
+        return "";
+    }
+  };
 
   return (
     <HeroUINavbar isMenuOpen={isMenuOpen} className="text-white bg-burgundy">
@@ -23,27 +44,42 @@ export const Navbar = () => {
         />
         <NavbarBrand>
           {/* You can add your logo or brand name here */}
+          <span className="sm:hidden flex justify-center w-full">
+            {getPageTitle()}
+          </span>
         </NavbarBrand>
       </NavbarContent>
 
       <NavbarContent className="hidden sm:flex gap-4" justify="center">
         <NavbarItem>
-          <Link className="text-white" href="/dashboard">
+          <Link
+            className={getLinkClass("/dashboard", "text-white")}
+            href="/dashboard"
+          >
             Dashboard
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className="text-white" href="/settings/connections">
+          <Link
+            className={getLinkClass("/settings/connections", "text-white")}
+            href="/settings/connections"
+          >
             Connections
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className="text-white" href="/settings/invite">
+          <Link
+            className={getLinkClass("/settings/invite", "text-white")}
+            href="/settings/invite"
+          >
             Invite
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link className="text-white" href="/settings">
+          <Link
+            className={getLinkClass("/settings", "text-white")}
+            href="/settings"
+          >
             Settings
           </Link>
         </NavbarItem>
